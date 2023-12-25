94e5b271069280d91f1ed7a2430f16eb0747de9f
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-12-26 00:11:48 +0530

    programm

--- 1.py
+++ 1.py
@@ -1,5 +1,31 @@
+# SIMPLE PYTHON PROGRAMM
 a=10;
 b=20;
 c=30;
 sum=a+b+c;
 print("Sum : ",sum);
+
+#  LIST AND TUPLE IN PYTHON 
+
+# LIST
+print("About list, List can modified but Tuple cannot");
+data=[10,20,-20,'A',"Ankush"];
+print(data);
+print(type(data));
+data[1]=30;
+print("Modified data : ",data);
+# TUPLE 
+data1=(10,20,-20,'S',"Saini");
+# data1[1]=30;         THIS WE CANNOT DO IN PYTHON 
+print(data1);
+
+# SET - UNORDERED COLLECTION OF DATA 
+
+s={10,20,30,10,20,"Ankush","Ankush","Ankush "};
+print(s);
+
+ 
+# MAP
+
+s1={100:"Ankush saini", 102:"Sahil saini", 103:"Ayush Sharma"};
+print(s1);
