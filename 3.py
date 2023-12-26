db0c62036e0ffbe0fce1fc8a59131c02c4155f20
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-12-27 00:45:57 +0530

    conversion

--- 3.py
+++ 3.py
@@ -1,14 +1,27 @@
 # MEMBERSHIP OPERATORS AND IDENTIFY OPERATORS
 # 1. in 2.not in
 # 1. is 2.is not
-print("Membership operators")
+print("Membership operators :")
 str="Welcome to SainiAnkushgithub"
 print("git"in str)
 print("rgit" in str)
 print("rgit"not in str)
+print()
 
-print("Identify operators")
+print("Identify operators :")
 a=10
 b=10
 print(a is b)
 print(a is not b)
+print()
+
+# TYPE CONVERSION 
+print("IMPLICIT TYPE CONVERSION :")
+a=10
+b=20
+c=a/b
+print(c)
+print(type(c))
+
+print(bin(a))
+print(oct(a))
