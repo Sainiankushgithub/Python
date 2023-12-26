99f4bbb9e3f4b07902a385d1390e4fede3c5303c
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-12-27 00:31:03 +0530

    operators

--- 2.py
+++ 2.py
@@ -1,4 +1,4 @@
-# ARITHEMATIC OPERATORS 
+#  OPERATORS 
 print("Arithmetic Operators ");
 a=10 ; b=20;
 print(a+b);
@@ -17,4 +17,13 @@ a+=20;
 print(a);
 
 print("Bitwise Operators ");
+
+print("AND OF 2 AND 3 :",2&3);
+print("OR OF 2 AND 3 :",2|3);
+print("Not of ~2:",~2);
+print("XOR of 2 AND 3 :",2^3);
+print("Left shift by 2 ");
+print("Left shift of 2 :",2<<2,"Left shift of 3 :",3<<2);
+print("Right shift by 2");
+print("Right shift of 2 :",2>>2,"Right shift of 3 :",3>>2);
 
