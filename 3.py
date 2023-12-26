ad3c62c299a56c9962bba1149954e2d0fed36a5c
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-12-27 00:40:08 +0530

    operators

---
+++ 3.py
@@ -0,0 +1,14 @@
+# MEMBERSHIP OPERATORS AND IDENTIFY OPERATORS
+# 1. in 2.not in
+# 1. is 2.is not
+print("Membership operators")
+str="Welcome to SainiAnkushgithub"
+print("git"in str)
+print("rgit" in str)
+print("rgit"not in str)
+
+print("Identify operators")
+a=10
+b=10
+print(a is b)
+print(a is not b)
