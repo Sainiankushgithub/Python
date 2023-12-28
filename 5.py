43794d846f03cee73cc56fccb10b402db30dfbd3
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-12-28 23:38:42 +0530

    program

---
+++ 5.py
@@ -0,0 +1,15 @@
+# LOOPS IN PYTHON 
+i=0
+while(i<10):
+    print(i)
+    i=i+1
+
+
+print("Printing the even and odd numbers form nth natural number")
+i=0
+num=int(input("Enter the nth natural number "))
+print("Displaying all the Even number form all natural number")
+while(i<num):
+    if(i%2==0):
+        print(i)
+    i=i+1
