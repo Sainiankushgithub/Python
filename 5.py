a3bcf8c9ff904bc7d01d63975411e7598527b19a
sainiankushgithub <www.ankushsaini55@gmail.com>
2023-12-30 12:51:14 +0530

    reverse number

--- 5.py
+++ 5.py
@@ -1,15 +1,24 @@
 # LOOPS IN PYTHON 
-i=0
-while(i<10):
-    print(i)
-    i=i+1
+# i=0
+# while(i<10):
+#     print(i)
+#     i=i+1
 
 
-print("Printing the even and odd numbers form nth natural number")
-i=0
-num=int(input("Enter the nth natural number "))
-print("Displaying all the Even number form all natural number")
-while(i<num):
-    if(i%2==0):
-        print(i)
-    i=i+1
+# print("Printing the even and odd numbers form nth natural number")
+# i=0
+# num=int(input("Enter the nth natural number "))
+# print("Displaying all the Even number form all natural number")
+# while(i<num):
+#     if(i%2==0):
+#         print(i)
+#     i=i+1
+
+n=int(input("Enter the number to gets reverse "))
+sum=0
+while(n>0):
+    r=n%10
+    sum=sum*10+r;
+    n=n//10;
+
+print("Reverse of your number is ",sum);
